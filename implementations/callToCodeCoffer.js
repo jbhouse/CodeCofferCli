797d58f2818e9fdc1a08430d132111da83264511
jbhouse <jbmoorhouse1@gmail.com>
2019-12-27 00:47:46 -0500

    master: initial commit. code coffer users can now have their preferences set up and import snippets

---
+++ implementations/callToCodeCoffer.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+"use strict";
+const projectBaseDirectory = __dirname.split("\\implementations")[0];
+const snippetBaseDirectory = '.\\snippets\\';
+const userConfigFilePath = projectBaseDirectory + "\\" + "userConfig.json";
+const userConfiguration = require(userConfigFilePath);
+const snippet_1 = require("../models/snippet");
+const clipboardy = require('clipboardy');
+const https = require('https');
+const fs = require('fs');
+
+function openFileInEditor(snippet) {
+    let filePaths = transformSupplementNamesToFilePaths(snippet).join(" ");
+    require('child_process').exec(userConfiguration.defaultEditor + " " + filePaths);
+}
+function transformSupplementNamesToFilePaths(snippet) {
+    const snippetDirectory = projectBaseDirectory + snippetBaseDirectory + snippet.title;
+    return snippet.supplements
+        .map(supplement => supplement.name + '.' + supplement.language)
+        .map(fileName => '"' + snippetDirectory + "\\" + fileName + '"');
+}
+function saveFile() {
+    return (snippet) => {
+        let snippetDirectory = projectBaseDirectory + snippetBaseDirectory + snippet.title;
+        console.log(snippetDirectory);
+        if (!fs.existsSync(snippetDirectory)) {
+            fs.mkdirSync(snippetDirectory, { recursive: true });
+            snippet.supplements.forEach(supplement => {
+                let fileName = supplement.name + '.' + supplement.language;
+                fs.writeFileSync(snippetDirectory + "\\" + fileName, supplement.code);
+            });
+        }
+        else {
+            fs.readdirSync(snippetDirectory).forEach((file) => {
+                fs.readFile(snippetDirectory + "\\" + file, 'utf8', function (err, contents) {
+                    let incomingSnippetContents = snippet.supplements.filter(supplement => supplement.name + '.' + supplement.language === file)[0].code;
+                    if (contents != incomingSnippetContents) {
+                        const rl = readline.createInterface({
+                            input: process.stdin,
+                            output: process.stdout
+                        });
+                        rl.question("'" + file + "' already exists and differs from snippet being imported. Replace the existing snippet? (Y/N): ", (answer) => {
+                            if (answer.toLocaleLowerCase() == 'y' || answer.toLocaleLowerCase() == 'yes') {
+                                fs.writeFileSync(snippetDirectory + "\\" + file, incomingSnippetContents);
+                            }
+                            rl.close();
+                        });
+                    }
+                });
+            });
+        }
+    };
+}
+function saveContentToClipboard(snippet) {
+    clipboardy.writeSync(snippet.supplements.map(supplement => supplement.code).join("\n"));
+}
+function getSnippet(url, callbacks) {
+    https.get(url, (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        resp.on('end', () => {
+            const snip = snippet_1.Snippet.createValidSnippet(JSON.parse(data)[0].message.content);
+            callbacks.forEach(callback => {
+                callback(snip);
+            });
+        });
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+}
+(function openSnippetFromUrl(userInput, fs) {
+    const id = userInput.split("import/")[1];
+    const url = "https://jayman-gameserver.herokuapp.com/conversations/" + id + "?startingIndex=0";
+    let callbacks = [saveFile(fs)];
+    if (userConfiguration.openFilesOnImport) {
+        callbacks.push(openFileInEditor);
+    }
+    if (userConfiguration.copyContentsToClipBoard) {
+        callbacks.push(saveContentToClipboard);
+    }
+    getSnippet(url, callbacks);
+})(process.argv.slice(2).join(" "))
---
+++ implementations/configureCodeCofferUser.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+"use strict";
+const fs = require('fs');
+const readline = require('readline');
+const projectBaseDirectory = __dirname.split("\\implementations")[0];
+const userConfigFilePath = projectBaseDirectory + "\\" + "userConfig.json";
+const userConfiguration = require(userConfigFilePath);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const openFilesOnImport = () => {
+    return new Promise((resolve, reject) => {
+        rl.question("\nWould you like to open snippets when they are imported? (Y/N) \n" + "Currently " + userConfiguration.openFilesOnImport + ": ", (answer) => {
+            updateUserConfig(answer, 'openFilesOnImport');
+            resolve();
+        });
+    });
+};
+const copyToClipboard = () => {
+    return new Promise((resolve, reject) => {
+        rl.question("\nWould you like to have snippet contents save to your clipboard? (Y/N) \n" + "Currently " + userConfiguration.copyContentsToClipBoard + ": ", (answer) => {
+            updateUserConfig(answer, 'copyContentsToClipBoard');
+            resolve();
+        });
+    });
+};
+const assignDefaultEditorPath = () => {
+    return new Promise((resolve, reject) => {
+        rl.question("\nYour current editor for opening snippets is: " + userConfiguration.defaultEditor + "\n" + "define the path to the code editor we should open imported/existing snippets in: ", (answer) => {
+            userConfiguration.defaultEditor = answer;
+            resolve();
+        });
+    });
+};
+function updateUserConfig(answer, userPropertyToUpdate) {
+    if (answer.toLocaleLowerCase() == 'y' || answer.toLocaleLowerCase() == 'yes') {
+        userConfiguration[userPropertyToUpdate] = true;
+    }
+    if (answer.toLocaleLowerCase() == 'n' || answer.toLocaleLowerCase() == 'no') {
+        userConfiguration[userPropertyToUpdate] = false;
+    }
+}
+
+(async function (userConfigFilePath) {
+    console.log("\nLeave the input empty for any of the following to keep your current setting");
+    await openFilesOnImport();
+    await copyToClipboard();
+    await assignDefaultEditorPath();
+    rl.close();
+    userConfiguration.userIsConfigured = true;
+    fs.writeFileSync(userConfigFilePath, JSON.stringify(userConfiguration, null, 2));
+})(userConfigFilePath);
